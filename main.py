fa10d77cba44b8c81b80796a5b2ed1a40f7d908c
Josh Grand <8271819+jgrand-gh@users.noreply.github.com>
2025-02-17 13:04:41 -0600

    updated main.py for solution parity

--- main.py
+++ main.py
@@ -7,18 +7,14 @@ from constants import *
 def main():
     pygame.init()
     game_window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    BLACK = (0, 0, 0)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        game_window.fill(BLACK)
+        
+        game_window.fill("black")
         pygame.display.flip()
-
-    #print("Starting asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
